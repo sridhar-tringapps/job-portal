882836a79e7625c7d52872acf38c265bb1911b1a
sridhar16899 <sridhar.arumugam@tringapps.com>
2021-04-27 15:49:38 +0530

    db to postgres

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
   resources :jobs
   resources :job_applications
-  devise_for :users
-  get 'users/index'
-  get 'users/sign_out'
-  root to: 'jobs#index'
+  devise_scope :user do
+    authenticated :user do
+      root 'jobs#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   namespace :api do
     namespace :v1 do
       resources :jobs
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_04_25_171228) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "job_applications", force: :cascade do |t|
     t.integer "user_id"
     t.integer "job_id"
